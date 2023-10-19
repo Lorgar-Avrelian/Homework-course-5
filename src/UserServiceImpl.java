562bb7d03101184297caba6ff464642f369af77b
Lorgar-Avrelian <victor-14-244@mail.ru>
2023-10-19 17:24:28 +0300

    Homework initialization

---
+++ src/UserServiceImpl.java
@@ -0,0 +1,20 @@
+import java.util.Collection;
+
+public class UserServiceImpl implements UserService{
+    @Override
+    public Collection<User> deleteDuplicate(Collection<User> userCollection) {
+        return null;
+    }
+    @Override
+    public Collection<User> sortByName(Collection<User> userCollection) {
+        return null;
+    }
+    @Override
+    public Collection<User> sortByAge(Collection<User> userCollection) {
+        return null;
+    }
+    @Override
+    public Collection<User> returnOld(Collection<User> userCollection) {
+        return null;
+    }
+}
