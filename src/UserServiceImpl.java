5f170974ba2cc21e636c5e09865b8748cd915fb2
Lorgar-Avrelian <victor-14-244@mail.ru>
2023-10-19 18:12:08 +0300

    Homework initialization

--- src/UserServiceImpl.java
+++ src/UserServiceImpl.java
@@ -1,20 +1,43 @@
 import java.util.Collection;
+import java.util.Comparator;
 
 public class UserServiceImpl implements UserService{
     @Override
     public Collection<User> deleteDuplicate(Collection<User> userCollection) {
-        return null;
+        return userCollection.stream()
+                .distinct()
+                .toList();
     }
     @Override
     public Collection<User> sortByName(Collection<User> userCollection) {
-        return null;
+        return userCollection.stream()
+                .sorted(Comparator.comparing(User::getName))
+                .toList();
     }
     @Override
     public Collection<User> sortByAge(Collection<User> userCollection) {
-        return null;
+        return userCollection.stream()
+                .sorted(Comparator.comparing(User::getAge))
+                .toList();
     }
     @Override
-    public Collection<User> returnOld(Collection<User> userCollection) {
-        return null;
+    public User oldest(Collection<User> userCollection) {
+        return userCollection.stream()
+                .sorted(Comparator.comparing(User::getAge).reversed())
+                .toList()
+                .stream()
+                .findFirst()
+                .get();
+    }
+    @Override
+    public User doAllTasks(Collection<User> userCollection) {
+        return userCollection.stream()
+                .distinct()
+                .sorted(Comparator.comparing(User::getName))
+                .sorted(Comparator.comparing(User::getAge).reversed())
+                .toList()
+                .stream()
+                .findFirst()
+                .get();
     }
 }
